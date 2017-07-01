85c82a36e0982351247f1affe0fe338187da2d50
duncanmccoll <duncan.mccoll97@gmail.com>
2017-07-01 14:03:47 +1000

    + fixed CSS

--- Day 3/app.js
+++ Day 3/app.js
@@ -3,11 +3,12 @@ $(document).ready(function() {
     function randomHex() {
         return '#' + Math.floor(Math.random() * 16777215).toString(16);
     }
-    
+
     $("button").click(function() {
         $("body").css("background-color", randomHex());
+        
        var bg = $("body").css("background-color");
        console.log(bg);
     });
-    
+
 });
